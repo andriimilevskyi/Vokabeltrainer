c159c8475f35548cc413db7f5799fe9252bf136f
amilevskyi <amilevskyi@D493900BD7E9>
2026-06-11 14:00:08 +0200

    Projektdateien hinzufügen.

---
+++ Vokabeltrainer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(new string('-', 10));
+        Console.WriteLine("Vokabeltrainer");
+        Console.WriteLine("MILEVSKYI");
+        Console.WriteLine(new string('-', 10));
+        Console.ResetColor();
+
+
+    }
+}
