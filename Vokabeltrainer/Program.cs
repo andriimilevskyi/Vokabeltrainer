00df0770e1756f2bcb6c18792cf4a546d5755559
Andrii Milevskyi <amilevskyi@viona.rocks>
2026-06-11 15:57:11 +0200

    Implement basic menu structur

--- Vokabeltrainer/Program.cs
+++ Vokabeltrainer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 
 public class Program
@@ -13,5 +14,45 @@ public class Program
         Console.ResetColor();
 
 
+        Hashtable vocabluary = new Hashtable();
+
+        while (true) 
+        {
+
+            Console.WriteLine("\n=== Vokabeltrainer ===");
+            Console.WriteLine("1 - Vokabel hinzufügen");
+            Console.WriteLine("2 - Vokabeln anzeigen");
+            Console.WriteLine("3 - Lernen");
+            Console.WriteLine("4 - Beenden");
+            Console.Write("Auswahl: ");
+
+            var choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    
+                    break;
+
+                case "2":
+                    
+                    break;
+
+                case "3":
+                    
+                    break;
+
+                case "4":
+                    Console.WriteLine("Programm beendet.");
+                    return;
+
+                default:
+                    Console.WriteLine("Ungültige Auswahl!");
+                    break;
+            }
+        }
+
     }
+
+}
 }
